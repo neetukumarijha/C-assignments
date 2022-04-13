96e9287fb97440df5f36b429d6d13b4cf5b3cc07
neetukumari <53509943+neetukumarijha@users.noreply.github.com>
2022-04-13 05:44:35 -0700

    Add files via upload

---
+++ 1st assignment C#/circumference.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace circumference
+{
+    class CirlceAreaCircumference
+    {
+        static void calculateAreaCircumference()
+        {
+            double radius, area, circumference;
+            const double PI = 3.14;
+            Console.Write("\nEnter the radius of circle ");
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            circumference = 2 * PI * radius;
+            Console.WriteLine("Circumference of Circle : {0}", circumference);
+
+            area = PI * radius * radius;
+            Console.WriteLine("\nThe area of circle is : {0}", area);
+            Console.ReadLine();
+
+        }
+
+        static void Main(String[] args)
+        {
+            CirlceAreaCircumference.calculateAreaCircumference();
+        }
+    }
+}
